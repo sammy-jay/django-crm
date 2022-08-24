f8110d1536306d9e2ceb4cf4257224e51060eb79
sammy-jay <sammyjayden27@gmail.com>
2022-08-24 03:03:08 +0100

    added more function based views

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from .models import Lead, Agent
+from .forms import LeadForm
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+
+    return render(request, 'leads/lead_list.html', context)
+
+def lead_detail(request, pk):
+    lead = get_object_or_404(Lead, id=pk)
+    # lead = Lead.objects.get(id=pk)
+    context = {"lead": lead}
+
+    return render(request, 'leads/lead_detail.html', context)
+
+def lead_create(request):
+    form = LeadForm()
+
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads/')
+
+    context = {'form': form}
+    return render(request, 'leads/lead_create.html', context) 
