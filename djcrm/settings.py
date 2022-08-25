326345e96572c95096342cc2e982572abe470bd4
sammy-jay <sammyjayden27@gmail.com>
2022-08-25 16:05:48 +0100

    added MEDIA_URL and MEDIA_ROOT to settings.py, added the path to urls.py too

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = "static_root"
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
