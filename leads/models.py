36b869b73312f163c49e2aca69224c8782518f87
sammy-jay <sammyjayden27@gmail.com>
2022-08-26 01:00:47 +0100

    added more data

--- leads/models.py
+++ leads/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
