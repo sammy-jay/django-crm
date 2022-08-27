1f8b878baf5a3658586e96c3351e74ac4d8451ad
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-27 13:12:25 +0000

    made the admin page much better

--- leads/admin.py
+++ leads/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Lead, User, Agent
+from .models import Lead, User, Agent, UserProfile
 
-admin.site.register(User)
+
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'age',
+                    'agent')
+    fields = ['first_name', 'last_name',
+              'agent', ('image', 'age'), 'description']
+    list_filter = ('agent', 'updated_at')
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active',
+                    )
+    list_filter = ('is_active', 'is_staff')
+    fields = ['username', 'first_name', 'last_name',
+              'email', 'password', ('is_active', 'is_staff')]
+    #   , 'groups', 'user_permissions'
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Agent)
-admin.site.register(Lead)
+admin.site.register(Lead, LeadAdmin)
+admin.site.register(UserProfile)
